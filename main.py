3ac63a791bd5e03d1401b2a6f72b041e81ee380e
DakoOlexiniozfetg365343 <129578271+DakoOlexiniozfetg365343@users.noreply.github.com>
2023-04-08 10:18:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 class Cat:
+    isHungry = True
+
     def __init__(self,name):
         self.name = name
 
@@ -13,14 +15,20 @@ class Person:
         self.name = name
         self.age = age
 
+    def feed(self, cat):
+        if cat.isHungry:
+            print(self.name, "is feeding cat named", cat.name)
+            cat.isHungry = False
+        else:
+            print(cat.name, "is not hungry")
+
+
+
 
 my_cat = Cat("Barsik")
 me = Person("Sasha", 13)
 friend = Person("Vania", 14)
+streat_cat = Cat("Olik")
+me.feed(streat_cat)
 
-print(me.isHappy)
-
-my_cat.play(me)
-my_cat.play(friend)
-
-print(me.isHappy)
+me.feed(my_cat)
